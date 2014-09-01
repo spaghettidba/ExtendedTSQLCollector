47bb7848319a93b6298e7807a939a17a02099c12
spaghettidba_cp <SND\spaghettidba_cp>
2014-09-01 20:55:38 +0000

    UI enhancements

--- GenericTSQLCollector/CollectionSetManager/Main.Designer.cs
+++ GenericTSQLCollector/CollectionSetManager/Main.Designer.cs
@@ -83,6 +83,7 @@
             this.treeView1.Size = new System.Drawing.Size(334, 502);
             this.treeView1.TabIndex = 0;
             this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
             // 
             // panel6
             // 
